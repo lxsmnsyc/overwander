a1265808f367e0f8f0dcafbc24a8dc358bd7e82b
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2026-09-18 11:57:05 +0800

    add: wellspring cave

--- src/data/species/gen-5/index.ts
+++ src/data/species/gen-5/index.ts
@@ -1,12 +1,15 @@
 import registerBlitzleSpecies from './blitzle';
+import registerDrilburSpecies from './drilbur';
 import registerLillipupSpecies from './lillipup';
 import registerMunnaSpecies from './munna';
 import registerOshawottSpecies from './oshawott';
 import registerPatratSpecies from './patrat';
 import registerPidoveSpecies from './pidove';
+import registerRoggenrolaSpecies from './roggenrola';
 import registerPurrloinSpecies from './purrloin';
 import registerSnivySpecies from './snivy';
 import registerTepigSpecies from './tepig';
+import registerWoobatSpecies from './woobat';
 
 export default function registerGen5Species(): void {
   registerSnivySpecies();
@@ -18,4 +21,7 @@ export default function registerGen5Species(): void {
   registerMunnaSpecies();
   registerPidoveSpecies();
   registerBlitzleSpecies();
+  registerRoggenrolaSpecies();
+  registerWoobatSpecies();
+  registerDrilburSpecies();
 }
